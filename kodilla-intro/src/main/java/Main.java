c794591c84eb49caee82f1aff50217cfa4f25f7a
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-05-08 15:39:14 +0200

    MineExtended

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstClass = "Hello from FirstClass!";
+        System.out.println(firstClass);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String theOldestPigName = "Robin";
+        String theYoungestPigName = "Millie";
+        int howManyPigsDidIHaveInPeak = 9;
+        double totalWeightOfMyPigsInKg = 3.584;
+        char howManyGuineaPigsDoIHave = '5';
+        boolean doMyPigsLoveLettuce = true;
+        System.out.println(theOldestPigName);
+        System.out.println(theYoungestPigName);
+        System.out.println(howManyPigsDidIHaveInPeak);
+        System.out.println(totalWeightOfMyPigsInKg);
+        System.out.println(howManyGuineaPigsDoIHave);
+        System.out.println(doMyPigsLoveLettuce);
+    }
+}
