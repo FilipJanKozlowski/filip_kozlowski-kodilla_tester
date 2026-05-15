13988a230246c9aa15d80e72c7200238b0d2e61d
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-05-15 09:51:38 +0200

    Final version

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,5 +1,5 @@
 public class Loops {
-    public int sum(int[] numbers) {
+    public static int sum(int[] numbers) {
         int total = 0;
         for (int i = 0; i < numbers.length; i++) {
             total = total + numbers[i];
@@ -7,9 +7,8 @@ public class Loops {
         return total;
     }
     public static void main(String[] args) {
-        Loops loops = new Loops();
         int[] myNumbers = {2, 4, 6, 8, 10};
-        int result = loops.sum(myNumbers);
+        int result = sum(myNumbers);
         System.out.println("Suma elementow tablicy to: " + result);
     }
 }
