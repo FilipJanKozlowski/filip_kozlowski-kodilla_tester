e2a026c12564039b092f5b3f58d8be17ce5fc0f9
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-06-28 09:41:33 +0200

    Test

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -4,9 +4,9 @@ public class Application {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
-        int sumResult = calculator.sum(a, b);
+        double a = 5;
+        double b = 8;
+        double sumResult = calculator.sum(a, b);
         boolean correctSum = ResultChecker.assertEquals(13, sumResult);
         if (correctSum) {
             System.out.println("Metoda sum dziala poprawnie dla liczb " + a + " i " + b);
@@ -14,7 +14,7 @@ public class Application {
             System.out.println("Metoda sum nie dziala poprawnie dla liczb " + a + " i " + b);
         }
 
-        int subtractResult = calculator.subtract(a, b);
+        double subtractResult = calculator.subtract(a, b);
         boolean correctSubstract = ResultChecker.assertEquals(-3, subtractResult);
         if (correctSubstract) {
             System.out.println("Metoda substract dziala poprawnie dla liczb " + a + " i " + b);
@@ -22,7 +22,7 @@ public class Application {
             System.out.println("Metoda substract nie dziala poprawnie dla liczb " + a + " i " + b);
         }
 
-        int squareResult = calculator.square(a);
+        double squareResult = calculator.square(a);
         boolean correctSquare = ResultChecker.assertEquals(25, squareResult);
         if (correctSquare) {
             System.out.println("Metoda square dziala poprawnie dla liczb " + a + " i " + a);
--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -9,9 +9,42 @@ public class CalculatorTestSuite {
     @Test
     public void testSum() {
         Calculator calculator = new Calculator();
-        int a = 5;
-        int b = 8;
-        int sumResult = calculator.sum(a, b);
-        assertEquals(13, sumResult);
+        double a = 5;
+        double b = 8;
+        double sumResult = calculator.sum(a, b);
+        assertEquals(13.0, sumResult, 0.001);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        double a = 5;
+        double b = 8;
+        double subtractResult = calculator.subtract(a, b);
+        assertEquals(-3.0, subtractResult, 0.001);
+    }
+
+    @Test
+    public void testSquarePositive() {
+        Calculator calculator = new Calculator();
+        double a = 5.0;
+        double squareResult = calculator.square(a);
+        assertEquals(25.0, squareResult, 0.001);
+    }
+
+    @Test
+    public void testSquareNegative() {
+        Calculator calculator = new Calculator();
+        double a = -5.0;
+        double squareResult = calculator.square(a);
+        assertEquals(25.0, squareResult, 0.001);
+    }
+
+    @Test
+    public void testSquareZero() {
+        Calculator calculator = new Calculator();
+        double a = 0.0;
+        double squareResult = calculator.square(a);
+        assertEquals(0.0, squareResult, 0.001);
     }
 }
