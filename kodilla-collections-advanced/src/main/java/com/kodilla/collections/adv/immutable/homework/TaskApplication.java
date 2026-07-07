58677298e08baacb4ad884dc6f7f886aa0c7a281
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-07-07 11:27:08 +0200

    immutability

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
@@ -0,0 +1,11 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class TaskApplication {
+    public static void main(String[] args) {
+        Task task1 = new Task("Learning about immutability in Java", 4);
+        Task task2 = new Task("Learning about maps in Java", 2);
+        Task task3 = new Task("Learning about tests in Java", 3);
+    }
+}
