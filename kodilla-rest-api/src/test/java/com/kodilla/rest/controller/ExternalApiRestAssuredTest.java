0227af1f2ad63b05e9f0802d143e743e99014968
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-08-20 00:05:03 +0200

    REST API 1

---
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/ExternalApiRestAssuredTest.java
@@ -0,0 +1,51 @@
+package com.kodilla.rest.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class ExternalApiRestAssuredTest {
+
+    @Test
+    void testGetPostShouldReturnStatus200AndValidateResponse() {
+        given().  // [1]
+                header("Content-Type", "application/json").
+                when().
+                get("https://jsonplaceholder.typicode.com/posts/1").  // [2]
+                then().
+                assertThat().
+                statusCode(200).  //  [3]
+                body("userId", equalTo(1)).  //  [4]
+                body("id", equalTo(1)).
+                body("title", equalTo("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")).
+                body("body", equalTo("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")).
+                log().all();  // [5]
+    }
+
+    @Test
+    void testUpdatePostShouldReturnStatus200AndValidateResponse() {
+        //given
+        String requestBody = "{"
+                + "\"id\": 1,"
+                + "\"title\": \"foo\","
+                + "\"body\": \"bar\","
+                + "\"userId\": 1"
+                + "}";
+
+        //when & then
+        given().
+                header("Content-Type", "application/json").
+                body(requestBody).
+                when().
+                put("https://jsonplaceholder.typicode.com/posts/1").
+                then().
+                assertThat().
+                statusCode(200).
+                body("id", equalTo(1)).
+                body("title", equalTo("foo")).
+                body("body", equalTo("bar")).
+                body("userId", equalTo(1)).
+                log().all();
+    }
+}
