8f19396cf07dba8a73f9f468b25812cf0562a5e3
Filip Kozlowski <kozlowskiglowny@wp.pl>
2026-08-24 13:11:02 +0200

    EBay

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -4,25 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EBayTestingApp {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "C:\\selenium-drivers\\Chrome\\chromedriver.exe");
-
         WebDriver driver = new ChromeDriver();
-
         driver.get("https://www.ebay.com/");
 
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        WebElement searchField = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.id("gh-ac"))
-        );
-
-        searchField.sendKeys("Laptop");
-
-        searchField.submit();
+        WebElement inputField = driver.findElement(By.name("_nkw"));
+        inputField.sendKeys("Laptop");
+        inputField.submit();
     }
 }
